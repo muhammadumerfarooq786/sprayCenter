0f85e34cccc3e93a828ce6e7d490b21e0508e312
Muhammad Umer Farooq <45636911+muhammadumerfarooq786@users.noreply.github.com>
2021-04-17 22:54:50 +0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,173 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <title>Spray Center</title>
+  <link rel="shortcut icon" type="image/ico" href="images/favicon.ico"/>
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Montserrat|Ubuntu" rel="stylesheet">
+
+  <!-- CSS Stylesheets -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+  <link rel="stylesheet" href="css/style.css">
+
+  <!-- Font Awesome -->
+  <script defer src="https://use.fontawesome.com/releases/v5.0.7/js/all.js"></script>
+
+  <!-- Bootstrap Scripts -->
+  <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+</head>
+
+<body>
+  <section class="colored-section" id="title">
+
+  <div class="container-fluid">
+
+    <!-- Nav Bar -->
+
+    <nav class="navbar navbar-expand-lg navbar-dark">
+
+      <a class="navbar-brand" href="">Ar Rahman</a>
+
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02">
+          <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item">
+            <a class="nav-link" href="pages/addingNew.php">Adding New</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="pages/openingBalance.html">Opening Balance</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="pages/dailyActivities.html">Daily Activities</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="pages/booksAndLedges.html">Books And Ledges</a>
+          </li>
+        </ul>
+
+      </div>
+    </nav>
+
+
+    <div class="row">
+
+      <div class="col-lg-6">
+        <h1 class="big-heading">Ar Rahman Spray Center</h1>
+        <h4 class="sub-heading">Sitting mistake towards his few country ask. You delighted two rapturous six depending objection happiness something the. Off nay impossible dispatched partiality unaffected. Norland adapted put ham cordial. Ladies talked may shy bask
+          </h4>
+      </div>
+
+      <div class="col-lg-3">
+        <img class="title-image" src="images/farmer.png" alt="farmer">
+      </div>
+
+      <div class="col-lg-3">
+        <img class="title-image2" src="images/ladyFarmer.png" alt="ladyFarmer">
+      </div>
+
+    </div>
+
+  </div>
+
+</section>
+
+
+
+
+<!-- Features -->
+
+<section class="white-section" id="features">
+
+  <div class="container-fluid">
+
+    <div class="row">
+      <div class="feature-box col-lg-4">
+        <i class="icon fas fa-check-circle fa-4x"></i>
+        <h3 class="feature-title">Easy to use.</h3>
+        <p>So easy to use, even your son could do it.</p>
+      </div>
+
+      <div class="feature-box col-lg-4">
+        <i class="icon fas fa-bullseye fa-4x"></i>
+        <h3 class="feature-title">Easy to Manage</h3>
+        <p>So easy to use, even your son could do it.</p>
+      </div>
+
+      <div class="feature-box col-lg-4">
+        <i class="icon fas fa-heart fa-4x"></i>
+        <h3 class="feature-title">Guaranteed to work.</h3>
+        <p>So easy to use, even your son could do it.</p>
+      </div>
+    </div>
+
+
+  </div>
+</section>
+
+
+
+
+  <!-- Testimonials -->
+
+  <section class="colored-section" id="testimonials">
+
+    <div id="testimonial-carousel" class="carousel slide" data-ride="false">
+      <div class="carousel-inner">
+        <div class="carousel-item active container-fluid">
+          <h2 class="testimonial-text">Owner</h2>
+          <img class="testimonial-image" src="images/man1.png" alt="dog-profile">
+
+        </div>
+        <div class="carousel-item container-fluid">
+          <h2 class="testimonial-text">Owner 2</h2>
+          <img class="testimonial-image" src="images/man2.png" alt="lady-profile">
+
+        </div>
+      </div>
+      <a class="carousel-control-prev" href="#testimonial-carousel" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon"></span>
+      </a>
+      <a class="carousel-control-next" href="#testimonial-carousel" role="button" data-slide="next">
+    <span class="carousel-control-next-icon"></span>
+      </a>
+    </div>
+
+  </section>
+
+
+  <!-- Press -->
+
+  <section class="white-section" id="press">
+    <img class="press-logo" src="images/TechCrunch.png" alt="tc-logo">
+    <img class="press-logo" src="images/tnw.png" alt="tnw-logo">
+    <img class="press-logo" src="images/bizinsider.png" alt="biz-insider-logo">
+    <img class="press-logo" src="images/mashable.png" alt="mashable-logo">
+
+  </section>
+
+
+
+  <footer class="white-section" id="footer">
+  <div class="container-fluid">
+    <a href="https://www.youtube.com/" target="_blank"><i class="social-icon fab fa-facebook-f"></i> </a>
+
+    <a href="https://www.youtube.com/" target="_blank"><i class="social-icon fab fa-twitter"></i></a>
+    <a href="https://www.youtube.com/" target="_blank"><i class="social-icon fab fa-instagram"></i></a>
+    <a href="https://www.youtube.com/" target="_blank"><i class="social-icon fas fa-envelope"></i></a>
+    <p class="footer-text">© Copyright 2021  Ar - Rahman Spray Center</p>
+  </div>
+</footer>
+
+
+</body>
+
+</html>
